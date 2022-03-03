948d94b5f5d8309fa91ea243f69a29f4e4027844
Haidau Alina <alina.haidau@students.info.uaic.ro>
2022-03-03 12:27:15 +0200

    Laborator 1 update

--- Laborator1/Compulsory/src/com/company/Main.java
+++ Laborator1/Compulsory/src/com/company/Main.java
@@ -11,7 +11,10 @@ public class Main {
             System.out.println("Usage: number, number, one or more characters");
             System.exit(-1);
         } else if (!lab1.isPositiveAndNumeric(args[0]) || !lab1.isPositiveAndNumeric(args[1])) { //if the first 2 arguments are numbers
-            System.out.println("Only positive numbers");
+            System.out.println("First two arguments have to be positive numbers");
+            System.exit(-1);
+        } else if (!lab1.areLetters(args)){
+            System.out.println("After two numbers is required only letters");
             System.exit(-1);
         }
         lab1.homework(args);
@@ -22,10 +25,12 @@ public class Main {
         System.out.println("Hello World!");
         String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
         int n = (int) (Math.random() * 1_000_000);
+
         n *= 3;
-        n += 0b10101 + 0xFF;
+        n += Integer.parseInt("10101", 2) + Integer.parseInt("FF", 16);
         n *= 6;
         int result = 0;
+
         while (n > 0 || result > 9) {
             if (n == 0) {
                 n = result;
@@ -45,12 +50,12 @@ public class Main {
         int p = Integer.parseInt(args[1]);
         int m = args.length - 2;
         char[] alphabet = new char[m];
-        for (int i = 0; i < m; i++) { //take the rest of the arguments
+        for (int i = 0; i < m; i++) {
             alphabet[i] = args[i + 2].charAt(0);
         }
 
         String[] words = new String[n];
-        if (n > 30_000) {
+        if (n >= 10_000) {
             for (int i = 0; i < n; i++) {
                 words[i] = createRandWord(alphabet, p);
             }
@@ -85,10 +90,10 @@ public class Main {
             }
         }
 
-        if (n > 30_000) {
+        if (n >= 10_000) {
             long endTime = System.nanoTime();
             long totalTime = endTime - startTime;
-            System.out.println("Running time of the application" + totalTime);
+            System.out.println("Running time of the application: " + totalTime);
         } else {
             System.out.println("Neighbour array: ");
             for (int i = 0; i < n; i++) {
@@ -101,18 +106,35 @@ public class Main {
         }
     }
 
+    //functia va verifica daca un string dat ca argument este un numar pozitiv, iar pentru asta am folosit metoda
+    //isLetter al clasei Character care va returna false daca sirul va contine litere, si true daca sirul este un numar
+    //Daca primul caracter al sirului este '-', asta poate insemna ca este un numar negativ, insa nu putem aloca memorie
+    //de numar negativ deci functia returneaza false
     boolean isPositiveAndNumeric(String word) {
         if (word.charAt(0) == '-') {
             return false;
         } else {
             for (int i = 0, n = word.length(); i < n; i++) {
-                if (!Character.isDigit(word.charAt(i)))
+                if (Character.isLetter(word.charAt(i)))
                     return false;
             }
         }
         return true;
     }
 
+    //functia va verifica daca restul parametrilor sunt doar litere, iar in cazul in care am gasit o cifra se va returna false
+    boolean areLetters(String[] args) {
+        for (int i = 0, m = args.length - 2; i < m; i++) {
+            char letter = args[i + 2].charAt(0);
+            if (Character.isDigit(letter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //functia foundCommonLetter va parcurge fiecare caracter al primul argument si va verifica daca cel de-al doilea
+    //argument contine va contine caracterul respectiv
     boolean foundCommonLetter(String wordOne, String wordTwo) {
         for (int i = 0, len = wordOne.length(); i < len; i++) {
             char l = wordOne.charAt(i);
@@ -135,6 +157,6 @@ public class Main {
     }
 
     void bonus() {
-        System.out.println("Bonus not implemented yet");
+        System.out.println("Bonus not implemented");
     }
 }
