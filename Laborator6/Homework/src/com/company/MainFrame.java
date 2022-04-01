651eceba6e1bb44f2076479359c8f206bf11516e
Haidau Alina <alina.haidau@students.info.uaic.ro>
2022-04-01 11:17:54 +0300

    Laborator 6 Comp Update + Homework

---
+++ Laborator6/Homework/src/com/company/MainFrame.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import javax.swing.*;
+
+import static java.awt.BorderLayout.*;
+
+public class MainFrame extends JFrame {
+    ConfigPanel configPanel;
+    ControlPanel controlPanel;
+    DrawingPanel canvas;
+
+    public MainFrame() {
+        super("Laborator 6");
+        init();
+    }
+
+    private void init() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        configPanel = new ConfigPanel(this);
+        controlPanel = new ControlPanel(this);
+        canvas = new DrawingPanel(this);
+        add(canvas, CENTER);
+        add(configPanel, NORTH);
+        add(controlPanel, SOUTH);
+        pack();
+    }
+}
+
